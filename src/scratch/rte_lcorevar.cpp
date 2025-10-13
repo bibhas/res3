4cf7afa2d546136b4f109e03cea27f7196cb3c9e
Bibhas Acharya <mail@bibhas.com>
2025-10-13 12:00:52 +0100

    Updates rte_lcorevar.cpp with complete example.

--- src/scratch/rte_lcorevar.cpp
+++ src/scratch/rte_lcorevar.cpp
@@ -1,39 +1,57 @@
-#include <rte_common.h>
+// rte_lcorevar.cpp
+
 #include <iostream>
 #include <rte_lcore.h>
 #include <rte_lcore_var.h>
 #include <rte_per_lcore.h>
 
-struct foo_lcore_state {
-  int a;
-  long b;
+#define EXPECT(X, MSG) if (!(X)) { rte_panic(MSG); }
+
+struct workerstate_t {
+  int stateval = 0;
 };
 
-static RTE_LCORE_VAR_HANDLE(struct foo_lcore_state, lcore_states);
+static RTE_LCORE_VAR_HANDLE(workerstate_t, lcore_workerstates);
 
-long foo_get_a_plus_b(void) {
-  const struct foo_lcore_state *state = RTE_LCORE_VAR(lcore_states);
-  return state->a + state->b + 2;
+static int lcore_hello(__rte_unused void *arg) {
+  unsigned lcore_id = rte_lcore_id();
+  printf("Hello from lcore: %u\n", lcore_id);
+  workerstate_t *state = RTE_LCORE_VAR(lcore_workerstates);
+  printf("  The value of lcore_val `lcore_workerstates` is: %i\n", state->stateval);
+  return 0;
 }
 
-RTE_INIT(rte_foo_init) {
-  std::cout << "rte_foo_init..." << std::endl;
-  RTE_LCORE_VAR_ALLOC(lcore_states);
-
+RTE_INIT(workerstate_init) { // Called before main()
+  // Allocate lcore variable
+  RTE_LCORE_VAR_ALLOC(lcore_workerstates);
+  // Initialize variable for all lcores
   unsigned int lcore_id;
-  struct foo_lcore_state *state;
-
-  RTE_LCORE_VAR_FOREACH(lcore_id, state, lcore_states) {
-    state->a = lcore_id;
-    state->b = lcore_id * 10;
-    std::cout << "id:" << lcore_id << " a:" << state->a << " b:" << state->b << std::endl;
+  workerstate_t *instance;
+  RTE_LCORE_VAR_FOREACH(lcore_id, instance, lcore_workerstates) {
+    instance->stateval = lcore_id * 100 + 1;
   }
 }
 
 int main(int argc, char **argv) {
-  printf("entering main...\n");
-  rte_eal_init(argc, argv);
-  printf("Sum on main lcore = %ld\n", foo_get_a_plus_b());
-  return 0;
-}
+  // Initialize EAL
+  int resp = rte_eal_init(argc, argv);
+  EXPECT(resp >= 0, "rte_eal_init failed");
+  
+  // Invoke `lcore_hello` in each of the lcores
+  unsigned lcore_id = 0;
+  RTE_LCORE_FOREACH_WORKER(lcore_id) {
+    rte_eal_remote_launch(lcore_hello, NULL, lcore_id);
+  }
 
+  // Wait for all worker lcores
+  rte_eal_mp_wait_lcore();
+
+  // Invoke same function in the main lcore directly
+  lcore_hello(NULL);
+
+  // Clean up EAL
+  rte_eal_cleanup();
+  
+  // And, we're done.
+  return resp;
+}
