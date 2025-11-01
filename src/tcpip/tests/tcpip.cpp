49a8cd836fc9323ead9f598ab3c7c53b0c2708c6
Bibhas Acharya <mail@bibhas.com>
2025-11-01 10:28:46 +0545

    Adds ring and linear topologies as utility functions.

--- src/tcpip/tests/tcpip.cpp
+++ src/tcpip/tests/tcpip.cpp
@@ -13,26 +13,26 @@
 
 graph_t *build_first_topo() {
   graph_t *topo = graph_init("Hello World Generic Graph");
+  // Add nodes
   node_t *R0_re = graph_add_node(topo, "R0_re");
   node_t *R1_re = graph_add_node(topo, "R1_re");
   node_t *R2_re = graph_add_node(topo, "R2_re");
-
+  // link nodes
   link_nodes(R0_re, R1_re, "eth0/0", "eth0/1", 1);
   link_nodes(R1_re, R2_re, "eth0/2", "eth0/3", 1);
   link_nodes(R0_re, R2_re, "eth0/4", "eth0/5", 1);
-
+  // R0_re  
   node_set_loopback_address(R0_re, "122.1.1.0");
   node_set_interface_ipv4_address(R0_re, "eth0/4", "40.1.1.1", 24);
   node_set_interface_ipv4_address(R0_re, "eth0/0", "20.1.1.1", 24);
-
+  // R1_re
   node_set_loopback_address(R1_re, "122.1.1.1");
   node_set_interface_ipv4_address(R1_re, "eth0/1", "20.1.1.2", 24);
   node_set_interface_ipv4_address(R1_re, "eth0/2", "30.1.1.1", 24);
-
+  // R2_re
   node_set_loopback_address(R2_re, "122.1.1.2");
   node_set_interface_ipv4_address(R2_re, "eth0/3", "30.1.1.2", 24);
   node_set_interface_ipv4_address(R2_re, "eth0/5", "40.1.1.2", 24);
-
   return topo;
 }
 
