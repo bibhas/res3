b5d3b6f157b32e11c39f34df707de5f57098d78c
Bibhas Acharya <mail@bibhas.com>
2025-11-24 04:57:54 +0000

    Adds working routing table impl using compressed binary trie. All tests passing.

--- src/tcpip/layer3/rt_cbtrie.cpp
+++ src/tcpip/layer3/rt_cbtrie.cpp
@@ -82,6 +82,23 @@ static inline rt_node_t* rt_node_allocate(rt_t *t, uint32_t prefix, uint8_t mask
   return resp;
 }
 
+static inline void rt_node_entry_deallocate(rt_t *t, rt_entry_t **entry_ptr) {
+  glthread_remove(&((*entry_ptr)->rt_glue));
+  free(*entry_ptr);
+  *entry_ptr = nullptr;
+}
+
+static inline void rt_node_deallocate(rt_t *t, rt_node_t **node_ptr) {
+  if (!node_ptr || !*node_ptr) {
+    return;
+  }
+  if ((*node_ptr)->entry != nullptr) {
+    rt_node_entry_deallocate(t, &((*node_ptr)->entry));
+  }
+  free(*node_ptr);
+  *node_ptr = nullptr;
+}
+
 static inline uint32_t rt_node_count_children(rt_node_t *n, rt_node_t **last_child = nullptr) {
   uint32_t resp = 0;
   for (int i = 0; i < RT_RADIX; i++) {
@@ -98,7 +115,9 @@ static inline uint32_t rt_node_count_children(rt_node_t *n, rt_node_t **last_chi
 static inline bool rt_insert_entry(rt_t *t, rt_entry_t *entry) {
   EXPECT_RETURN_BOOL(t != nullptr, "Empty rt param", false);
   EXPECT_RETURN_BOOL(entry != nullptr, "Empty entry param", false);
-  uint32_t entry_prefix = UINT32_MASK(entry->prefix.addr.value, entry->prefix.mask);
+  uint32_t _prefix = htonl(entry->prefix.addr.value);
+  uint32_t __prefix = ntohl(entry->prefix.addr.value);
+  uint32_t entry_prefix = UINT32_MASK(htonl(entry->prefix.addr.value), entry->prefix.mask);
   uint32_t entry_mask = entry->prefix.mask;
   if (t->root_node == nullptr) {
     t->root_node = rt_node_allocate(t, entry_prefix, entry_mask, entry);
@@ -126,7 +145,7 @@ static inline bool rt_insert_entry(rt_t *t, rt_entry_t *entry) {
       uint32_t diverged_bitval = UINT32_READ_BIT(curr_node->prefix, i);
       split_node->child_nodes[diverged_bitval] = curr_node;
       if (i < entry_mask) {
-        split_node->child_nodes[diverged_bitval] = rt_node_allocate(t, entry_prefix, entry_mask, entry);
+        split_node->child_nodes[!diverged_bitval] = rt_node_allocate(t, entry_prefix, entry_mask, entry);
       }
       curr_node = split_node;
       if (parent_node) {
@@ -180,11 +199,65 @@ bool rt_add_direct_route(rt_t *t, ipv4_addr_t *addr, uint8_t mask) {
   return rt_add_route(t, addr, mask, nullptr, nullptr, true);
 }
 
-bool rt_delete_entry(rt_t *t, ipv4_addr_t *addr, uint8_t mask) {
+bool rt_delete_entry(rt_t *t, ipv4_addr_t *entry_addr, uint8_t entry_mask) {
   EXPECT_RETURN_BOOL(t != nullptr, "Empty rt param", false);
-  EXPECT_RETURN_BOOL(addr != nullptr, "Empty destination ip address param", false);
-  // ...
-  return false;
+  EXPECT_RETURN_BOOL(entry_addr != nullptr, "Empty destination ip address param", false);
+  if (!t->root_node) { return false; }
+  rt_node_t *curr_node = t->root_node;
+  rt_node_t **parent_node_ptr_stack[33] = {0};
+  int32_t parent_node_ptr_stack_depth = 0;
+  parent_node_ptr_stack[parent_node_ptr_stack_depth++] = &t->root_node;
+  uint32_t entry_prefix = UINT32_MASK(htonl(entry_addr->value), (uint32_t)entry_mask);
+  for (;;) {
+    for (int i = 0; i < std::min(curr_node->prefixlen, (uint32_t)entry_mask); i++) {
+      if (UINT32_READ_BIT(curr_node->prefix, i) != UINT32_READ_BIT(entry_prefix, i)) {
+        return false;
+      }
+    }
+    if (curr_node->prefix == entry_prefix && curr_node->prefixlen == entry_mask) {
+      if (curr_node->entry == nullptr) {
+        return true;
+      }
+      if (rt_node_count_children(curr_node) == 0) {
+        rt_node_t **parent_ptr = parent_node_ptr_stack[parent_node_ptr_stack_depth - 1];
+        *parent_ptr = nullptr;
+        rt_node_deallocate(t, &curr_node);
+      }
+      else if (rt_node_count_children(curr_node) > 1) {
+        if (curr_node->entry != nullptr) {
+          rt_node_entry_deallocate(t, &curr_node->entry);
+        }
+        return true;
+      }
+      else {
+        rt_node_entry_deallocate(t, &curr_node->entry);
+      }
+      // Compress
+      while (parent_node_ptr_stack_depth > 0) {
+        rt_node_t **parent_ptr = parent_node_ptr_stack[--parent_node_ptr_stack_depth];
+        rt_node_t *parent_node = *parent_ptr;
+        if (parent_node == nullptr) { continue; }
+        if (parent_node->entry != nullptr) { continue; }
+        rt_node_t *child_node = nullptr;
+        if (rt_node_count_children(parent_node, &child_node) == 1) {
+          *parent_ptr = child_node;
+          rt_node_deallocate(t, &parent_node);
+        }
+      }
+      return true;
+    }
+    if (entry_mask < curr_node->prefixlen) {
+      return false;
+    }
+    uint32_t diverged_bitval = UINT32_READ_BIT(entry_prefix, curr_node->prefixlen);
+    if (curr_node->child_nodes[diverged_bitval] != nullptr) {
+      parent_node_ptr_stack[parent_node_ptr_stack_depth++] = &curr_node->child_nodes[diverged_bitval];
+      curr_node = curr_node->child_nodes[diverged_bitval];
+    }
+    else {
+      return false; // Avoids looping forever (if we can't find the right node)
+    }
+  }
 }
 
 bool rt_clear(rt_t *t) {
@@ -197,15 +270,68 @@ bool rt_lookup_exact(rt_t *t, ipv4_addr_t *addr, uint8_t mask, rt_entry_t **resp
   EXPECT_RETURN_BOOL(t != nullptr, "Empty table param", false);
   EXPECT_RETURN_BOOL(addr != nullptr, "Empty address param", false);
   EXPECT_RETURN_BOOL(resp != nullptr, "Empty resp ptr param", false);
-  // ...
-  return false;
+  uint32_t query_prefix = UINT32_MASK(htonl(addr->value), mask);
+  uint32_t query_mask = mask;
+  if (t->root_node == nullptr) {
+    return false;
+  }
+  rt_node_t *curr_node = t->root_node;
+  for (;;) {
+    int i = 0;
+    for (; i < std::min(curr_node->prefixlen, query_mask); i++) {
+      if (UINT32_READ_BIT(curr_node->prefix, i) != UINT32_READ_BIT(query_prefix, i)) {
+        return false;
+      }
+    }
+    if (curr_node->prefix == query_prefix && curr_node->prefixlen == query_mask) {
+      if (curr_node->entry == nullptr) {
+        return false; // Intermediary node
+      }
+      *resp = curr_node->entry;
+      return true;
+    }
+ 
+      uint32_t diverged_bitval = UINT32_READ_BIT(query_prefix, i);
+      curr_node = curr_node->child_nodes[diverged_bitval];
+      if (!curr_node) {
+        return false;
+      }
+
+  } 
 }
 
 bool rt_lookup(rt_t *t, ipv4_addr_t *addr, rt_entry_t **resp) {
   EXPECT_RETURN_BOOL(t != nullptr, "Empty table param", false);
   EXPECT_RETURN_BOOL(addr != nullptr, "Empty address param", false);
   EXPECT_RETURN_BOOL(resp != nullptr, "Empty resp entry ptr ptr param", false);
-  // ...
+  uint32_t query_prefix = htonl(addr->value);
+  if (t->root_node == nullptr) {
+    return false;
+  }
+  rt_node_t *curr_node = t->root_node;
+  rt_node_t *candidate_stack[33] = {0};
+  int16_t candidate_stack_depth = -1;
+  for (;;) {
+    int i = 0;
+    for (; i < curr_node->prefixlen; i++) {
+      if (UINT32_READ_BIT(curr_node->prefix, i) != UINT32_READ_BIT(query_prefix, i)) {
+        goto ret_false;
+      }
+    }
+    if (curr_node->entry != nullptr) {
+      candidate_stack[++candidate_stack_depth] = curr_node;
+    }
+    uint32_t diverged_bitval = UINT32_READ_BIT(query_prefix, i);
+    curr_node = curr_node->child_nodes[diverged_bitval];
+    if (!curr_node) {
+      goto ret_false;
+    }
+  } 
+ret_false:
+  if (candidate_stack_depth > -1) {
+    *resp = candidate_stack[candidate_stack_depth]->entry;
+    return true;
+  }
   return false;
 }
 
